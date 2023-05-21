8460bd16261d9bcf8f96b163022ec3abf51b3e33
mtenb <info@mtenb.nl>
2023-05-21 15:32:26 +0200

    refactor

--- building/building.py
+++ building/building.py
@@ -15,24 +15,6 @@ from dataclasses import dataclass, field
 from typing import Dict, Optional, Tuple
 from handcalcs.decorator import handcalc
 
-# def geometric_data(geom_floor, height):
-#         """
-#         Returns a dictionary with nodes, edges and faces, 
-#         representing the 3d geometry of the object.
-#         'nodes' # [[x, y, z],...] nodes in x, y, z
-#         'edges' # [[i, j],...] meaning node numbers
-#         'faces' # [[i, j, k],...] meaning node numbers
-#         """
-#         nodes, edges, faces = convert_geom_definition(
-#             geom_floor(), 
-#             height
-#         )
-#         geom_data = {
-#             'nodes': nodes, 
-#             'edges': edges,
-#             'faces': faces
-#         }
-#         return geom_data
 
 @dataclass
 class Building_geometry:
@@ -48,29 +30,6 @@ class Building_geometry:
     pd_wind: float = Optional[float] # kN/m2
     
     
-
-    def geom_building_floor(self):
-        """
-        Returns a dictionary representing 2d geometry of the building ground floor.
-        'nodes_floor' # [[x, y],...] nodes in x, y on ground level
-        'edges_floor' # [[i, j],...] meaning nodes numbers on ground level
-        """
-        nodes_floor = [[0, 0], [self.width, 0], [self.width, self.depth], [0, self.depth]]
-        edges_floor = [[0, 1], [1, 2], [2, 3], [3, 0]] 
-          
-        geom_building = {
-            'nodes_floor': nodes_floor, 
-            'edges_floor': edges_floor
-        }
-        
-        # geom_data = geometric_data(geom_building, self.height)
-        # geom_data['faces'] += add_roof_faces(nodes, edges)
-        # self.geom_data = geom_data
-        return geom_building
-    
-    
-    
-    
     def geom_building(self):
         """
         Returns a dictionary with nodes, edges and faces, 
@@ -78,11 +37,16 @@ class Building_geometry:
         'nodes' # [[x, y, z],...] nodes in x, y, z
         'edges' # [[i, j],...] meaning node numbers
         'faces' # [[i, j, k],...] meaning node numbers
+    
+        N.B.:
+        'nodes_floor' # [[x, y],...] nodes in x, y on ground level
+        'edges_floor' # [[i, j],...] meaning nodes numbers on ground level
         """
-        nodes, edges, faces = convert_geom_definition(
-            self.geom_building_floor(), 
-            self.height
-        )
+        nodes_floor = [[0, 0], [self.width, 0], [self.width, self.depth], [0, self.depth]]
+        edges_floor = [[0, 1], [1, 2], [2, 3], [3, 0]] 
+        
+        nodes, edges, faces = convert_geom_definition(nodes_floor, edges_floor, self.height)
+        
         faces += add_roof_faces(nodes, edges)
         geom_building = {
             'nodes': nodes, 
@@ -90,7 +54,7 @@ class Building_geometry:
             'faces': faces
         }
         return geom_building
-    
+        
     
 @dataclass
 class Shearwall:
@@ -105,52 +69,45 @@ class Shearwall:
     bot_flange_height: int = 250 # mm
     aligned: str = 'left' # 'left', 'center' or 'right'
     height: float = 25.0 # m
-    # geom_data = geometric_data(self.geom_shearwall_floor(), self.height)
     
-    def geom_shearwall_floor(self):
-        """
-        Returns a dictionary representing 2d geometry of the shearwall.
-        'nodes_floor' # [[x, y],...] nodes in x, y on ground level
-        'edges_floor' # [[i, j],...] meaning nodes numbers on ground level
-        """
-        h = (
-            (self.top_flange_height / 2) / 1000 + 
-            self.web_height / 1000 + 
-            (self.bot_flange_height / 2) / 1000
-        )
-        
-        if self.aligned == 'center':
-            nodes = [[-(self.top_flange_width / 2) / 1000, h], [0, h], [self.top_flange_width / 1000, h]]
-            nodes += [[-(self.bot_flange_width / 2) / 1000, 0], [0, 0], [self.bot_flange_width / 1000, 0]]
-            edges = [[0, 1], [1, 2], [3, 4], [1, 4]]
-        elif self.aligned == 'left':
-            nodes = [[0, h], [self.top_flange_width / 1000, h]]
-            nodes += [[0, 0], [self.bot_flange_width / 1000, 0]]
-            edges = [[0, 1], [2, 3], [0, 2]]
-        elif self.aligned == 'right':
-            nodes = [[0, h], [self.top_flange_width / 1000, h]]
-            nodes += [[0, 0], [self.bot_flange_width / 1000, 0]]
-            edges = [[0, 1], [2, 3], [1, 3]]
-        else:
-            print("Error. No valid alignment given")
-        geom_shearwall = {
-            'nodes_floor': nodes, 
-            'edges_floor': edges
-        }
-        return geom_shearwall
-
+    
     def geom_shearwall(self):
         """
         Returns a dictionary with nodes, edges and faces, 
-        representing the 3d geometry of the shearwall.
+        representing the 3d geometry of a shearwall.
         'nodes' # [[x, y, z],...] nodes in x, y, z
         'edges' # [[i, j],...] meaning node numbers
         'faces' # [[i, j, k],...] meaning node numbers
+    
+        N.B.:
+        'nodes_floor' # [[x, y],...] nodes in x, y on ground level
+        'edges_floor' # [[i, j],...] meaning nodes numbers on ground level
         """
-        nodes, edges, faces = convert_geom_definition(
-            self.geom_shearwall_floor(), 
-            self.height
-        )
+        h = (self.top_flange_height / 2 + self.web_height + self.bot_flange_height / 2) / 1000
+        
+        if self.aligned == 'center':
+            nodes_floor = [[-(self.top_flange_width / 2) / 1000, h], [0, h], [self.top_flange_width / 1000, h]]
+            nodes_floor += [[-(self.bot_flange_width / 2) / 1000, 0], [0, 0], [self.bot_flange_width / 1000, 0]]
+            edges_floor = [[0, 1], [1, 2], [3, 4], [1, 4]]
+        elif self.aligned == 'left':
+            nodes_floor = [[0, h], [self.top_flange_width / 1000, h]]
+            nodes_floor += [[0, 0], [self.bot_flange_width / 1000, 0]]
+            edges_floor = [[0, 1], [2, 3], [0, 2]]
+        elif self.aligned == 'right':
+            nodes_floor = [[0, h], [self.top_flange_width / 1000, h]]
+            nodes_floor += [[0, 0], [self.bot_flange_width / 1000, 0]]
+            edges_floor = [[0, 1], [2, 3], [1, 3]]
+        else:
+            print("Error. No valid alignment given")
+        
+        
+        nodes, edges, faces = convert_geom_definition(nodes_floor, edges_floor, self.height)
+         
+        # self.nodes = nodes
+        # self.edges = edges
+        # self.faces = faces
+        # return
+    
         geom_shearwall = {
             'nodes': nodes, 
             'edges': edges,
@@ -158,13 +115,12 @@ class Shearwall:
         }
         return geom_shearwall
 
+
+
     
-def convert_geom_definition(geom_floor, building_h):
+def convert_geom_definition(nodes_floor, edges_floor, building_h):
     """
     """
-    nodes_floor = geom_floor['nodes_floor']
-    edges_floor = geom_floor['edges_floor']
-        
     # Add height to node coordinates
     nodes = [node_floor + [0] for node_floor in nodes_floor]
     nodes += [node_floor + [building_h] for node_floor in nodes_floor]
@@ -177,7 +133,7 @@ def convert_geom_definition(geom_floor, building_h):
     edges += [[idx, idx + start_roof_idx] for idx, node in enumerate(nodes_floor)]
     
     faces = []
-    for edge in geom_floor['edges_floor']:
+    for edge in edges_floor:
         faces.append(edge + [edge[0] + start_roof_idx])
         faces.append([edge[0] + start_roof_idx, edge[1] + start_roof_idx, edge[1]])
     return (nodes, edges, faces)
@@ -201,6 +157,7 @@ def plot_building(building, shearwalls: Optional= None):
     nodes = x['nodes']
     edges = x['edges']
     faces = x['faces']
+    st.write(faces)
     
     # plot building contour & faces
     plot_item_contour(fig, nodes, edges, color='rgb(0, 0, 255)', line_width=2, marker_size=2)
